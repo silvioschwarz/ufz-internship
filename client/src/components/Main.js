cead217de4b357e007573ec79af7921dae9f8190
Silvio Schwarz <admin@silvioschwarz.com>
2022-06-26 22:09:08 +0200

    loading animation
    conditionally overlay loading-div with spinner
    
    TODO: fix osmtogeojson vulnerabilities

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -3,11 +3,22 @@ import React from "react";
 import MapOpenLayers from "./openLayers/MapOpenLayers";
 import Sidebar from "./Sidebar";
 
-var osmtogeojson = require("osmtogeojson");
+import BeatLoader from "react-spinners/BeatLoader";
+
+// import styled, { css } from "styled-components";
+// import "../css/loader.css";
+
+
+// const ConditionalWrapper = ({ condition, wrapper, children }) => 
+//   condition ? wrapper(children) : children;
+
+
+const osmtogeojson = require('osmtogeojson');
 
 export default function Main() {
   const [showLayer1, setShowLayer1] = React.useState(false);
-  const isMounted = React.useRef(false)
+  const [loaded, setLoaded] = React.useState(true);
+  const isMounted = React.useRef(false);
 
   const [motorway, setMotorway] = React.useState(false);
   const [trunk, setTrunk] = React.useState(false);
@@ -19,36 +30,31 @@ export default function Main() {
 
   const bbox = [52.4, 13.25, 52.6, 13.4];
 
-  function handleRoadTypes(){
-    let roads = Array.from(document.querySelectorAll(
-      '#roadTypes input[type="checkbox"]:checked'
-    )).map((road) => {
-
-      if(road.value==="motorway"){
-        setMotorway(true)
+  function handleRoadTypes() {
+    let roads = Array.from(
+      document.querySelectorAll('#roadTypes input[type="checkbox"]:checked')
+    ).map((road) => {
+      if (road.value === "motorway") {
+        setMotorway(true);
       }
 
-      if(road.value==="trunk"){
-        setTrunk(true)
+      if (road.value === "trunk") {
+        setTrunk(true);
       }
 
-      if(road.value==="primary"){
-        setPrimary(true)
+      if (road.value === "primary") {
+        setPrimary(true);
       }
 
-
-      return(road.value)
+      return road.value;
     });
 
-    setRoadTypes(roads)
-
-   
-
+    setRoadTypes(roads);
 
     // setMotorway(prevState => !prevState)
     // setTrunk(prevState => !prevState)
     // setPrimary(prevState => !prevState)
-    };
+  }
 
   console.log(roadTypes);
 
@@ -57,9 +63,9 @@ export default function Main() {
   query += "[out:json][timeout:25];";
   query += "(";
 
-  roadTypes.map((road) =>{
-    query += `way["highway"="${road}"];`
-  })
+  roadTypes.map((road) => {
+    query += `way["highway"=${road}];`;
+  });
   // query += 'way["highway"="motorway"];';
   // query += 'node["leisure"]["access"!="private"]["sport"="swimming"];'
   // query += 'node["access"!="private"]["leisure"="swimming_pool"];'
@@ -70,36 +76,37 @@ export default function Main() {
   query += ")";
   query += ";out geom;>;";
 
-  console.log(query)
+  console.log(query);
 
   React.useEffect(() => {
-    if(isMounted.current){
-    setShowLayer1(false)
-    fetch("https://overpass-api.de/api/interpreter", {
-      method: "POST",
-      body: query,
-    })
-      .then((res) => {
-        console.log(res);
-        if (!res.ok) {
-          throw new Error("Network response was not OK");
-        }
-        return res.json();
+    if (isMounted.current) {
+      setLoaded(false);
+      setShowLayer1(false);
+      fetch("https://overpass-api.de/api/interpreter", {
+        method: "POST",
+        body: query,
       })
-      .then((json) => {
-        const geojson = osmtogeojson(json, {
-          flatProperties: true,
+        .then((res) => {
+          console.log(res);
+          if (!res.ok) {
+            throw new Error("Network response was not OK");
+          }
+          return res.json();
+        })
+        .then((json) => {
+          console.log(json)
+          const geojson = osmtogeojson(json);
+          console.log(geojson);
+          setGeoJSONObject(geojson);
+          setShowLayer1(true);
+          setLoaded(true);
+        })
+        .catch((error) => {
+          console.error(
+            "There has been a problem with your fetch operation:",
+            error
+          );
         });
-        console.log(geojson);
-        setGeoJSONObject(geojson);
-        setShowLayer1(true)
-      })
-      .catch((error) => {
-        console.error(
-          "There has been a problem with your fetch operation:",
-          error
-        );
-      });
     } else {
       isMounted.current = true;
     }
@@ -146,58 +153,67 @@ export default function Main() {
     <main>
       <p>Main Content</p>
       <div className="main-content">
-        <MapOpenLayers
-          showLayer1={showLayer1}
-          data={geoJSONObject}
-          bbox={bboxObject}
-        />
-        <aside>
-          <div>
-            <input
-              type="checkbox"
-              checked={showLayer1}
-              onChange={(event) => setShowLayer1(event.target.checked)}
-            />{" "}
-            Show Roads
-          </div>
-          <div>
-            <input type="file" />
-            <hr></hr>
-          </div>
-          <div id="roadTypes">
-            <h4>Road Types</h4>
-            <input
-              type="checkbox"
-              id="motorway"
-              name="motorway"
-              value="motorway"
-              checked={motorway}
-              onChange={handleRoadTypes}
-            />
-            <label htmlFor="motorway"> motorway</label>
-            <br />
-            <input
-              type="checkbox"
-              id="trunk"
-              name="trunk"
-              value="trunk"
-              checked={trunk}
-              onChange={handleRoadTypes}
-              />
-            <label htmlFor="trunk"> trunk</label>
-            <br />
-            <input
-              type="checkbox"
-              id="primary"
-              name="primary"
-              value="primary"
-              checked={primary}
-              onChange={handleRoadTypes}
-              />
-            <label htmlFor="primary"> primary</label>
-            <br />
-          </div>
-        </aside>
+       
+            <MapOpenLayers
+              showLayer1={showLayer1}
+              data={geoJSONObject}
+              bbox={bboxObject}
+            /> 
+            <div className="selection-div">
+            {!loaded && <div className="loading-div">
+              <BeatLoader />
+            </div>}   
+
+            <aside>
+              <div>
+                <input
+                  type="checkbox"
+                  checked={showLayer1}
+                  onChange={(event) => setShowLayer1(event.target.checked)}
+                />{" "}
+                Show Roads
+              </div>
+              <div>
+                <input type="file" />
+                <hr></hr>
+              </div>
+              <div id="roadTypes">
+                <h4>Road Types</h4>
+                <input
+                  type="checkbox"
+                  id="motorway"
+                  name="motorway"
+                  value="motorway"
+                  checked={motorway}
+                  onChange={handleRoadTypes}
+                />
+                <label htmlFor="motorway"> motorway</label>
+                <br />
+                <input
+                  type="checkbox"
+                  id="trunk"
+                  name="trunk"
+                  value="trunk"
+                  checked={trunk}
+                  onChange={handleRoadTypes}
+                />
+                <label htmlFor="trunk"> trunk</label>
+                <br />
+                <input
+                  type="checkbox"
+                  id="primary"
+                  name="primary"
+                  value="primary"
+                  checked={primary}
+                  onChange={handleRoadTypes}
+                />
+                <label htmlFor="primary"> primary</label>
+                <br />
+              </div>
+            </aside>
+
+            </div>
+
         {/* <Sidebar /> */}
       </div>
     </main>
